e68dc3b33cc004fe1a7fbceed33d568d3e70b08e
Journey36 <be9.smile@gmail.com>
2021-05-17 00:16:10 +0900

    Refactor - 코드 리팩토링
    [Descriptions]
    - 현재 URL 보여주기 위한 Delegate Method 추가
    - Namespace 삭제 및 AlertController 중복 함수화

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -1,23 +1,27 @@
 import UIKit
 import WebKit
 
-typealias ActionHandler = (UIAlertAction) -> Void
-
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     //MARK:- IBOutlets
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var urlSearchButton: UIButton!
-    
+    @IBOutlet weak var moveBackwardsButton: UIBarButtonItem!
+    @IBOutlet weak var moveForwardButton: UIBarButtonItem!
+
     //MARK:- IBActions
     @IBAction func moveToURL(_ sender: UIButton) {
-        guard let inputURLString = urlTextField.text,
+        guard var inputURLString = urlTextField.text,
               !inputURLString.isEmpty else {
-            checkURL()
+            presentErrorAlert(with: .emptyURL)
             return
         }
 
-        checkValidation(to: inputURLString)
+        if !checkValidation(to: inputURLString) {
+            inputURLString = "https://" + inputURLString
+        }
+
+        presentWebPage(inputURLString)
     }
     
     @IBAction func moveBackwards(_ sender: Any) {
@@ -32,24 +36,12 @@ class ViewController: UIViewController {
         webView.reload()
     }
 
-    //MARK:- View life cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setKeyboardAttribute()
-        webView.navigationDelegate = self
-        presentHomePage()
-    }
-}
+    //MARK:- Methods
+    private func presentWebPage(_ url: String) {
+        guard let targetURL = URL(string: url) else { return }
 
-//MARK:- Extensions
-//MARK: Methods
-extension ViewController {
-    private func presentHomePage() {
-        let homePage: String = "https://www.naver.com"
-
-        guard let homeUrl = URL(string: homePage) else { return }
-
-        let request = URLRequest(url: homeUrl)
+        let request = URLRequest(url: targetURL)
+        urlTextField.endEditing(true)
         webView.load(request)
     }
 
@@ -60,36 +52,43 @@ extension ViewController {
         urlTextField.clearButtonMode = UITextField.ViewMode.whileEditing
     }
 
-    private func setFirstResponder() -> ActionHandler? {
-        urlTextField.becomeFirstResponder()
-        return nil
-    }
-
-    private func checkURL() {
-        let errorMessage: String = "주소창에 이동하고자 하는 페이지의 주소를 입력해주세요."
-        let urlCheckingAlert = UIAlertController(title: Alert.title, message: errorMessage, preferredStyle: .alert)
-        let okAction: UIAlertAction = UIAlertAction(title: Action.ok, style: .default, handler: setFirstResponder())
-        urlCheckingAlert.addAction(okAction)
-        present(urlCheckingAlert, animated: true, completion: nil)
-    }
-
-    private func checkValidation(to url: String) {
+    private func checkValidation(to url: String) -> Bool {
         let range = NSRange(location: 0, length: url.count)
+        guard let regExpForValidURL = try? NSRegularExpression(pattern: "http(s)?://") else { return false }
 
-        guard let regExpForValidURL = try? NSRegularExpression(pattern: "http(s)?://") else { return }
+        return regExpForValidURL.firstMatch(in: url, range: range) != nil ? true : false
+    }
 
-        if regExpForValidURL.firstMatch(in: url, range: range) != nil {
-            guard let url = URL(string: url) else { return }
-            let request = URLRequest(url: url)
-            urlTextField.endEditing(true)
-            webView.load(request)
-        } else {
-            let errorMessage: String = "입력한 주소가 올바른 형태가 아닙니다. http:// 또는 https:// 를 주소에 포함시켜주세요!"
-            let invalidURLAlert = UIAlertController(title: Alert.title, message: errorMessage, preferredStyle: .alert)
-            let okAction: UIAlertAction = UIAlertAction(title: Action.ok, style: .default, handler: setFirstResponder())
-            invalidURLAlert.addAction(okAction)
-            present(invalidURLAlert, animated: true, completion: nil)
+    private func presentErrorAlert(with errorMessage: ErrorMessage) {
+        let title: String = "경고"
+        let alert: UIAlertController = UIAlertController(title: title, message: errorMessage.rawValue, preferredStyle: .alert)
+        let okAction: UIAlertAction = UIAlertAction(title: title, style: .default) { _ in
+            self.urlTextField.becomeFirstResponder()
         }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+    //MARK:- View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setKeyboardAttribute()
+        webView.navigationDelegate = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let homePage: String = "https://www.google.com"
+        presentWebPage(homePage)
+    }
+}
+
+//MARK:- Extensions
+//MARK: Methods
+extension ViewController {
+    enum ErrorMessage: String {
+        case emptyURL = "주소창에 이동하고자 하는 페이지의 주소를 입력해주세요."
+        case pageNotFound = "요청하신 페이지를 찾을 수 없습니다."
     }
 }
 
@@ -103,10 +102,12 @@ extension ViewController: UITextFieldDelegate {
 
 extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        let errorMessage: String = "요청하신 페이지를 찾을 수 없습니다."
-        let pageNotFoundAlert = UIAlertController(title: Alert.title, message: errorMessage, preferredStyle: .alert)
-        let okAction: UIAlertAction = UIAlertAction(title: Action.ok, style: .default, handler: setFirstResponder())
-        pageNotFoundAlert.addAction(okAction)
-        present(pageNotFoundAlert, animated: true, completion: nil)
+        presentErrorAlert(with: .pageNotFound)
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        urlTextField.text = webView.url?.absoluteString
+        moveBackwardsButton.isEnabled = webView.canGoBack
+        moveForwardButton.isEnabled = webView.canGoForward
     }
 }
